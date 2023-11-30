54de7a5c4f8a8facb14a790f6c1e56b0262f4ea0
gauravyadv <gauravyadavry_cse18@its.edu.in>
2023-11-30 17:26:39 +0530

    hi

---
+++ Backend/server.js
@@ -0,0 +1,77 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const axios = require('axios');
+const Data = require('./models/DataModel')
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Connect to MongoDB
+const uri = 'mongodb+srv://gauravyadava963998:k2PXTOAch2eswtuO@cluster0.yj6gutd.mongodb.net/FitPage?retryWrites=true&w=majority';
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1); // Exit the process if there's an error to prevent the server from starting
+  });
+
+// Configure CORS options
+
+// Apply CORS middleware to all routes
+app.use(cors());
+
+
+
+  app.get('/api/data/read', async (req, res) => {
+    try {
+      // Retrieve all data from the database
+      const dataObjects = await Data.find({});
+  
+      // Extract data from the retrieved objects
+      const savedData = [];
+      for (const dataObject of dataObjects) {
+        savedData.push(dataObject.data);
+      }
+  
+      // Return the retrieved data in the response
+      res.status(200).json({ data: savedData });
+    } catch (error) {
+      console.error('Error fetching data from MongoDB:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  app.get('/api/data', async (req, res) => {
+    try {
+      // Fetch data from the API
+      const apiUrl = 'http://coding-assignment.bombayrunning.com/data.json';
+      const response = await axios.get(apiUrl);
+      const dataArray = response.data;
+  
+      // Save data to MongoDB
+      const savedData = [];
+      for (const data of dataArray) {
+        const newData = new Data({ data: data });
+        await newData.save();
+        savedData.push(newData.data);
+      }
+  
+      res.status(200).json({ message: 'Data saved successfully', data :savedData });
+    } catch (error) {
+      console.error('Error fetching or saving data:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
