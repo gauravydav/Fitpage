414e7764242b2628c6701a957af4f88974ca26d3
gauravyadv <gauravyadavry_cse18@its.edu.in>
2023-11-30 17:39:31 +0530

    .

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,36 @@
+// HomePage.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import DataBox from './DataBox';
+
+const HomePage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/data');
+        const result = await response.json();
+        setData(result.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="phone-section">
+      {data.map((item) => (
+        <div key={item.id} style={{ marginBottom: '10px' }}>
+          <Link to={`/${item.name.toLowerCase().replace(/\s/g, '_')}`} style={{ textDecoration: 'none' }}>
+            <DataBox data={item} />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
